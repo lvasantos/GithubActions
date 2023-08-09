2c3f9ea0cfb5f97b331b55baf4a43dd395e9b39b
Luciana Adriao <lucianaasantos@hotmail.com>
2023-08-09 09:09:40 -0300

    🎉 First commit

---
+++ POC-GithubActions/POC_GithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_GithubActionsApp.swift
+//  POC-GithubActions
+//
+//  Created by Luciana Adrião on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_GithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
